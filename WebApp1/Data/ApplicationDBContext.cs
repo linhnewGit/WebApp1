fe52dea5218003ae65d9502658db4d848c51c841
Mai Van Linh <linh202987@gmail.com>
2023-11-23 09:18:07 +0700

    Add project files.

---
+++ WebApp1/Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp1.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+            
+        }
+    }
+}
